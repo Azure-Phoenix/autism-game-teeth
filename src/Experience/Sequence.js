9dc4453130f1b6785b2f7aab7e2d462bae4fd859
Azure-Phoenix <larrypotter0000@gmail.com>
2024-02-24 23:47:35 +0800

    fix: mobile event prevent

--- src/Experience/Sequence.js
+++ src/Experience/Sequence.js
@@ -45,7 +45,6 @@ export default class Sequence {
     this.finishResponseInterval = false
     this.responses = []
     this.scores = [100]
-    console.log(this.scores)
     this.gameScore = 0
     this.gameDuration = 0
     this.successInterection = 0
@@ -219,8 +218,8 @@ export default class Sequence {
           this.trigger_dragging(this.step)
         }
 
-        if (!this.autoAction.includes(this.step)) {
-          if (this.brushingAction.includes(this.step) && this.canControlBrushing) {
+        if (event.changedTouches[0].clientY == 1) {
+          if (this.brushingAction.includes(this.step)) {
             window.addEventListener("mousemove", this.brush)
             if (this.step == 8)
               this.experience.world.character.animation.actions.brushingURSK.play()
@@ -234,7 +233,7 @@ export default class Sequence {
               this.experience.world.character.animation.actions.brushingFSK.play()
           }
         } else {
-          if (this.brushingAction.includes(this.step)) {
+          if (this.brushingAction.includes(this.step) && this.canControlBrushing) {
             window.addEventListener("mousemove", this.brush)
             if (this.step == 8)
               this.experience.world.character.animation.actions.brushingURSK.play()
@@ -268,7 +267,13 @@ export default class Sequence {
           this.dragProgress.percent = 0
         }
 
-        window.removeEventListener("touchmove", this.brush)
+        if (this.canControlBrushing) {
+          window.removeEventListener("mousemove", this.brush)
+        } else {
+          if (event.clientY == 1) {
+            window.removeEventListener("mousemove", this.brush)
+          } else return
+        }
         window.removeEventListener("touchmove", this.dragging)
       })
     })
