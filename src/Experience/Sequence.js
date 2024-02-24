385e09340250f06141c70ccf5b9bedeff30b6a58
Azure-Phoenix <larrypotter0000@gmail.com>
2024-02-24 23:50:34 +0800

    fix: prompt Limit

--- src/Experience/Sequence.js
+++ src/Experience/Sequence.js
@@ -219,8 +219,8 @@ export default class Sequence {
           this.trigger_dragging(this.step)
         }
 
-        if (!this.autoAction.includes(this.step)) {
-          if (this.brushingAction.includes(this.step) && this.canControlBrushing) {
+        if (event.changedTouches[0].clientY == 1) {
+          if (this.brushingAction.includes(this.step)) {
             window.addEventListener("mousemove", this.brush)
             if (this.step == 8)
               this.experience.world.character.animation.actions.brushingURSK.play()
@@ -234,7 +234,7 @@ export default class Sequence {
               this.experience.world.character.animation.actions.brushingFSK.play()
           }
         } else {
-          if (this.brushingAction.includes(this.step)) {
+          if (this.brushingAction.includes(this.step) && this.canControlBrushing) {
             window.addEventListener("mousemove", this.brush)
             if (this.step == 8)
               this.experience.world.character.animation.actions.brushingURSK.play()
@@ -268,7 +268,13 @@ export default class Sequence {
           this.dragProgress.percent = 0
         }
 
-        window.removeEventListener("touchmove", this.brush)
+        if (this.canControlBrushing) {
+          window.removeEventListener("mousemove", this.brush)
+        } else {
+          if (event.changedTouches[0].clientY == 1) {
+            window.removeEventListener("mousemove", this.brush)
+          } else return
+        }
         window.removeEventListener("touchmove", this.dragging)
       })
     })
@@ -446,7 +452,7 @@ export default class Sequence {
 
   prompt_action(id) {
     this.promptLimit++
-    if (this.promptLimit == 2) {
+    if (this.promptLimit == 5) {
       // If user doesn't interact correctly during 4 prompts, do action automatically
       this.tempResponseTime = 0
       this.scores[this.scores.length - 1] = 100
